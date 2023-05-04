61b311fccf8eb9cd4eec90a59f40f068ce292725
JUorlando <110193846+JUorlando@users.noreply.github.com>
2023-05-04 17:33:18 -0300

    feat: rota follows

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from follows import views
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
     path("users/<int:pk>/", views.UserDetailView.as_view()),
     path("users/login/", views.LoginView.as_view()),
+    path("users/<int:pk>/follow/", views.FollowView.as_view()),
+    path("users/<int:pk>/follow/", views.FollowDestroyView.as_view()),
+    path("users/<int:pk>/follows/", views.FollowedListView.as_view()),
+    path("users/<int:pk>/following/", views.FollowListView.as_view())
 ]
