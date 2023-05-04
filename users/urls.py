0d390dc31dc4f4f0b82a7acb135ef9f2ee0648ed
rafaeuus <rafaeljcarvalhosilva@gmail.com>
2023-05-04 17:40:12 -0300

    feat: creating rotes to friends

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from . import views
+import friends.views as friends_views
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
     path("users/<int:pk>/", views.UserDetailView.as_view()),
     path("users/login/", views.LoginView.as_view()),
+    path(
+        "users/<int:pk>/friends/",
+        friends_views.CreateDestroyFriendRequestView.as_view(),
+    ),
+    path(
+        "users/friends/requests/",
+        friends_views.ListFriendRequestView.as_view(),
+    ),
+    path("users/friends/", friends_views.ListFriendView.as_view()),
 ]
