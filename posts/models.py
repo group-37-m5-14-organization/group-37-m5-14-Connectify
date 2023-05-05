83416602f7e7214904f18ce0447baeb359b8c265
JUorlando <110193846+JUorlando@users.noreply.github.com>
2023-05-05 10:59:28 -0300

    feat: ordenando por data do mais novo para o mais antigo

--- posts/models.py
+++ posts/models.py
@@ -8,3 +8,8 @@ class Post(models.Model):
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="posts"
     )
+    is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
