ab5a1c74de9042b27e050a1be59810b360dc9987
JUorlando <110193846+JUorlando@users.noreply.github.com>
2023-05-08 10:55:27 -0300

    feat/RoteUserRetrieveWithoutPermission

--- users/permissions.py
+++ users/permissions.py
@@ -5,4 +5,7 @@ from rest_framework.views import View
 
 class AccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User) -> bool:
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
         return request.user.is_authenticated and obj == request.user
