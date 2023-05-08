ca09454e53157e6c1ea3a9b63a1fd8cf30e6f796
José Orlando de Carvalho Júnior <110193846+JUorlando@users.noreply.github.com>
2023-05-08 18:33:25 -0300

    Update settings.py

--- connectify/settings.py
+++ connectify/settings.py
@@ -92,13 +92,15 @@ WSGI_APPLICATION = "connectify.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
-
-# DATABASE_URL = os.getenv("DATABASE_URL")
 
 if os.getenv("DATABASE_URL"):
     DATABASES["default"] = dj_database_url.config()
